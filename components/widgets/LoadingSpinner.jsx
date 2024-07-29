32ea68a2304f80278492191f5607650ae446223a
tw9str <i88b@hotmail.com>
2024-07-30 01:16:33 +0200

    improved ui

--- components/widgets/LoadingSpinner.jsx
+++ components/widgets/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex w-full items-center justify-center min-h-screen bg-gray-100">
       <div class="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
     </div>
   );
